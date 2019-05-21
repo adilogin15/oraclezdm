98720aa101ba3c12b61e8e36dc34de85bdc178b8
Aditya Sharma <adilogin@hotmail.com>
2019-05-21 20:09:09 +0530

    select menus complete minor changes

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Select from 'react-select';
+// import reset from './reset.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Body extends React.Component {
@@ -15,16 +16,9 @@ class Body extends React.Component {
         }
     }
 
-	migChange = (event) => {
-    console.log(event.target.id);
-
-    }
-    // toggleDisabled = () =>{
-    //     this.setState(state => ({ isDisabled: !state.isDisabled }));
-    // }
     componentDidMount() {
         var self = this;
-        console.log("The api is loaded");
+        // console.log("The api is loaded");
         this.setState(state => ({ isDisabled2: !state.isDisabled }));
         this.setState(state => ({ isDisabled3: !state.isDisabled }));
         this.setState(state => ({ isDisabled4: !state.isDisabled }));
@@ -42,39 +36,25 @@ class Body extends React.Component {
             config: { headers: {'Content-Type': 'multipart/form-data' }}
         }).then(function (response) 
         {
-            console.log(response.data[1],"first response");
+            // console.log(response.data[1],"first response");
             var rd= response.data[1];
             var keys = Object.keys(rd);
-            console.log("keys1 ---"+keys+"---- keys1");
+            // console.log("keys1 ---"+keys+"---- keys1");
             self.setState({
                 arrkey: keys,
                 seloptions: response.data
-            });
-            console.log("componentDidMountoptions", self.state.seloptions);
-            console.log("componentDidMountkeys", self.state.arrkey[0]);
-            // document.querySelectorAll(".css-1pcexqc-container")[1].style.visibility = "hidden";
-            // document.querySelectorAll(".css-1pcexqc-container")[2].style.visibility = "hidden";
-            // document.querySelectorAll(".css-1pcexqc-container")[3].style.visibility = "hidden";
-            // document.querySelectorAll(".css-1pcexqc-container")[4].style.visibility = "hidden";
-            // document.querySelectorAll(".css-1pcexqc-container")[5].style.visibility = "hidden";
-            // document.querySelectorAll(".css-1pcexqc-container")[6].style.visibility = "hidden";
-            // document.querySelectorAll(".css-1pcexqc-container")[7].style.visibility = "hidden";
-            // document.querySelectorAll(".css-1pcexqc-container")[8].style.visibility = "hidden";
-            // document.querySelectorAll(".css-1pcexqc-container")[9].style.visibility = "hidden";
-            // document.querySelectorAll(".css-1pcexqc-container")[10].style.visibility = "hidden";
-
-            
+            });         
         }).catch(function (response) {
             console.log(response);
         });
     }	
 
     onChangeFunc(selectedvalue, e) {
-        console.log("id : "+e)
-        console.log("this state is working");
-        console.log(selectedvalue);
+        // console.log("id : "+e)
+        // console.log("this state is working");
+        // console.log(selectedvalue);
         var self = this;
-        console.log("new response :"+ selectedvalue);
+        // console.log("new response :"+ selectedvalue);
         const value = selectedvalue.value;
         const label = selectedvalue.label;
         console.log(value+"   "+label);
@@ -86,7 +66,6 @@ class Body extends React.Component {
                     selections: e,
                     source_db_version : value,
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[1].style.visibility = "visible";
                 this.setState({ isDisabled2: false });
                 break;
             case "2":
@@ -95,7 +74,6 @@ class Body extends React.Component {
                     platform_family : value,
                     source_db_version : document.querySelectorAll(".css-1pcexqc-container .css-dvua67-singleValue")[0].innerText
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[2].style.visibility = "visible";
                 this.setState({ isDisabled3: false });
                 break;
             case "3":
@@ -105,7 +83,6 @@ class Body extends React.Component {
                     platform_family : document.querySelectorAll(".css-1pcexqc-container .css-dvua67-singleValue")[1].innerText,
                     os_platform: value
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[3].style.visibility = "visible";
                 this.setState({ isDisabled4: false });
                 break;
             case "4":
@@ -116,7 +93,6 @@ class Body extends React.Component {
                     os_platform: document.querySelectorAll(".css-1pcexqc-container .css-dvua67-singleValue")[2].innerText,
                     cdb : value
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[4].style.visibility = "visible";
                 this.setState({ isDisabled5: false });
                 break;
             case "5":
@@ -128,7 +104,6 @@ class Body extends React.Component {
                     cdb : document.querySelectorAll(".css-1pcexqc-container .css-dvua67-singleValue")[3].innerText,
                     nls_cs_compatible: value
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[5].style.visibility = "visible";
                 this.setState({ isDisabled6: false });
                 break;
             case "6":
@@ -141,7 +116,6 @@ class Body extends React.Component {
                     nls_cs_compatible: document.querySelectorAll(".css-1pcexqc-container .css-dvua67-singleValue")[4].innerText,
                     db_size: value
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[6].style.visibility = "visible";
                 this.setState({ isDisabled7: false });
                 break;
             case "7":
@@ -155,7 +129,6 @@ class Body extends React.Component {
                     db_size: document.querySelectorAll(".css-1pcexqc-container .css-dvua67-singleValue")[5].innerText,
                     network_bandwidth: value
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[7].style.visibility = "visible";
                 this.setState({ isDisabled8: false });
                 break;
             case "8":
@@ -170,7 +143,6 @@ class Body extends React.Component {
                     network_bandwidth: document.querySelectorAll(".css-1pcexqc-container .css-dvua67-singleValue")[6].innerText,
                     target_db: value
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[8].style.visibility = "visible";
                 this.setState({ isDisabled9: false });
                 break;
             case "9":
@@ -186,7 +158,6 @@ class Body extends React.Component {
                     target_db: document.querySelectorAll(".css-1pcexqc-container .css-dvua67-singleValue")[7].innerText,
                     target_db_version: value
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[9].style.visibility = "visible";
                 this.setState({ isDisabled10: false });
                 break;
             case "10":
@@ -203,8 +174,10 @@ class Body extends React.Component {
                     target_db_version: document.querySelectorAll(".css-1pcexqc-container .css-dvua67-singleValue")[8].innerText,
                     permissible_dt: value
                 }
-                // document.querySelectorAll(".css-1pcexqc-container")[10].style.visibility = "visible";
                 this.setState({ isDisabled11: false });
+                break;
+            case "11":
+                    document.querySelector('textarea[name="result"]').value = value;
                 break;
             default:
                 bodyFormData={
@@ -221,32 +194,40 @@ class Body extends React.Component {
             config: { headers: {'Content-Type': 'multipart/form-data' }}
         }).then(function (response) 
         {
-            console.log("helloonChangeFunc", response.data);
+            // console.log("helloonChangeFunc", response.data);
             var rd= response.data[0];
-            console.log(rd+"---------------rd")
             var keys = Object.keys(rd);
-            console.log("keys2 ---"+keys+"---- keys2");
+            // console.log("keys2 ---"+keys+"---- keys2");
             let newkeyArray = [];
             newkeyArray.push(response.data);
-            console.log(newkeyArray[0]);
+            // console.log(newkeyArray[0]);
             self.setState({
                 arrkey: keys,
                 seloptions: newkeyArray[0]
             });
-            console.log("componentDidMountoptions", self.state.seloptions[0]);
-            console.log("componentDidMountkeys", self.state.arrkey[0]);
-            console.log("helloonChangeFunc2", self.state);
         }).catch(function (response) {
             console.log(response);
         });
     }
-    
-    toggleDisabled = () =>this.setState(state => ({ isDisabled: !state.isDisabled }));
+    reset() {
+        this.setState({             
+            arrkey: [],
+            seloptions: [],
+            selectedvalue:null,
+            isDisabled: false
+        });
+    }
 	render() {
         // console.log("render options :"+options.value);
         // console.log("render key : "+this.state.arrkey);
         let options = this.state.seloptions.map(function (opt) {
-        return { value: opt.SOURCE_DB_VERSION, label: opt.SOURCE_DB_VERSION };
+            if(opt.SOURCE_DB_VERSION == null)
+            {
+                return { value: opt.SOURCE_DB_VERSION, label: opt.SOURCE_DB_VERSION , isDisabled : true};
+            }
+            else{
+                return { value: opt.SOURCE_DB_VERSION, label: opt.SOURCE_DB_VERSION};
+            }
         })
         let newoptions = this.state.seloptions.map(function (opt) {
             return { value: opt.PLATFORM_FAMILY, label: opt.PLATFORM_FAMILY };
@@ -281,7 +262,7 @@ class Body extends React.Component {
         const {isDisabled1,isDisabled2,isDisabled3,isDisabled4,isDisabled5,isDisabled6,isDisabled7,isDisabled8,isDisabled9,isDisabled10,isDisabled11} = this.state;
         return(
             <section className="cb110 cb110v0 cpad bkg-overlay bgvdarkgrey">
-                <div className="cb110w1 cwidth" >
+                <form className="cb110w1 cwidth" >
                     <Select options={options} onChange={(event) => this.onChangeFunc(event, "1")} isDisabled={isDisabled1} /><br/>
                     <Select options={newoptions} onChange={(event) => this.onChangeFunc(event, "2")} isDisabled={isDisabled2} /><br/>
                     <Select options={newoptions1} onChange={(event) => this.onChangeFunc(event, "3")} isDisabled={isDisabled3} /><br/>
@@ -292,8 +273,10 @@ class Body extends React.Component {
                     <Select options={newoptions6} onChange={(event) => this.onChangeFunc(event, "8")} isDisabled={isDisabled8} /><br/>
                     <Select options={newoptions7} onChange={(event) => this.onChangeFunc(event, "9")} isDisabled={isDisabled9} /><br/>
                     <Select options={newoptions8} onChange={(event) => this.onChangeFunc(event, "10")} isDisabled={isDisabled10} /><br/>
-                    <Select options={newoptions9} isDisabled={isDisabled11} />
-                </div>
+                    <Select options={newoptions9} onChange={(event) => this.onChangeFunc(event, "11")} isDisabled={isDisabled11} /><br/><br/>
+                    <span className="dropres">Result : </span><textarea id="dropresult" className="rwidth css-bg1rzq-control" name="result" title="Dropdown Result" /><br/>
+                    <div className="rwidth"><button onChange={this.reset.bind(this)}>Reset</button></div>
+                </form>
             </section>
 		)
 	}
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,6 +8,7 @@ class Footer extends React.Component {
                     <div className="u10w5">
                         <div className="u10-ologo">
                         <a className="o_icon_footer" href="/">Oracle</a></div>
+                        <div className="powered col-sm-12">Powered by Digital Tools	<span>&copy;</span></div>
                     </div>
                 </div>
             </div>
